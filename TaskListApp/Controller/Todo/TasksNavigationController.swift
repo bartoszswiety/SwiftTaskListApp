ce31ebeea38d6b09358b5e2a54cb114b786780f8
Bartosz Swiety <>
2019-12-06 17:49:55 +0100

    feat(tasklist): Initial Views

---
+++ TaskListApp/Controller/Todo/TasksNavigationController.swift
@@ -0,0 +1,32 @@
+//
+//  TodoNavigationController.swift
+//  TaskListApp
+//
+//  Created by Bartosz Swiety on 06/12/2019.
+//  Copyright © 2019 Bartosz Swiety. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class TasksNavigationController: UINavigationController
+{
+
+    public override func viewDidLoad() {
+
+
+        let v = TaskLitViewController()
+        setViewControllers([v], animated: true)
+
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.shadowImage = UIImage()
+        navigationBar.isTranslucent = true
+        view.backgroundColor = UIColor.clear
+        navigationBar.prefersLargeTitles = true
+        self.navigationItem.largeTitleDisplayMode = .always
+        self.navigationBar.setTitleVerticalPositionAdjustment(200, for: .default)
+        hidesBottomBarWhenPushed = true
+        setToolbarHidden(true, animated: false)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+}
---
+++ TaskListApp/View/TaskViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TaskViewCell.swift
+//  TaskListApp
+//
+//  Created by Bartosz Swiety on 06/12/2019.
+//  Copyright © 2019 Bartosz Swiety. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskViewCell: UITableViewCell
+{
+    public func style()
+    {
+        self.backgroundColor = .red
+    }
+}
