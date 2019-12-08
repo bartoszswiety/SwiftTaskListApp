34ebd88bfdc47abfbd3db3a5961ad01181b9d22a
Bartosz Swiety <>
2019-12-08 15:48:30 +0100

    feat(tasklist): Adding TodoItem model

--- TaskListApp/Model/DataModel/Todo.swift
+++ TaskListApp/Model/DataModel/Todo.swift
@@ -12,13 +12,14 @@ import CoreData
 @objc(Todo)
 public class Todo: NSManagedObject {
 
-    
+
     convenience init(title: String)
     {
         self.init(entity: NSEntityDescription.entity(forEntityName: "Todo", in: CoreDataStack.contex)!, insertInto: CoreDataStack.contex)
         self.setValue(title, forKey: "title")
     }
 }
+
 extension Todo {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Todo> {
@@ -28,9 +29,27 @@ extension Todo {
     @NSManaged public var createdAt: Date?
     @NSManaged public var createdBy: Int64
     @NSManaged public var id: Int64
+    @NSManaged public var test: Float
     @NSManaged public var title: String?
     @NSManaged public var updatedAt: Date?
-    @NSManaged public var test: Float
+    @NSManaged public var items: NSSet?
+
+}
+
+// MARK: Generated accessors for items
+extension Todo {
+
+    @objc(addItemsObject:)
+    @NSManaged public func addToItems(_ value: TodoItem)
+
+    @objc(removeItemsObject:)
+    @NSManaged public func removeFromItems(_ value: TodoItem)
+
+    @objc(addItems:)
+    @NSManaged public func addToItems(_ values: NSSet)
+
+    @objc(removeItems:)
+    @NSManaged public func removeFromItems(_ values: NSSet)
 
 }
 
